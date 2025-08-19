f9339ab57c92a387b2c38330e785940cf67a88d8
EstevaoA98 <estevao.a98@gmail.com>
2025-08-18 23:40:47 -0300

    feat: implementar a funcionalidade de criação de livro no BookController e adicionar métodos de repositório

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -2,17 +2,42 @@
 
 namespace App\Controller;
 
+use App\Entity\Book;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
+use App\Repository\BookRepository;
 
 final class BookController extends AbstractController
 {
-    #[Route('/books', name: 'app_book', methods: ['GET'])]
+    #[Route('/books', name: 'BookList', methods: ['GET'])]
     public function index(): Response
     {
         return $this->render('book/index.html.twig', [
             'controller_name' => 'BookController',
         ]);
     }
+
+    #[Route('/books', name: 'BooksCreate', methods: ['POST'])]
+    public function create(Request $request, BookRepository $bookRepository): JsonResponse
+    {
+        $data = $request->request->all();
+
+        $book = new Book();
+        $book->setTitle($data['title']);
+        $book->setIsbn($data['isbn']);
+        $book->setCreatedAt(new \DateTimeImmutable('now', new \DateTimeZone('America/Sao_Paulo')));
+        $book->setUpdatedAt(new \DateTimeImmutable('now', new \DateTimeZone('America/Sao_Paulo')));
+
+        $bookRepository->add($book, true);
+
+        return $this->json( [
+            'message' => 'Livro criado com sucesso!',
+            ]
+        , 204);
+    }
+        
 }
+
